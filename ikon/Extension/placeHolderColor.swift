615b5a47c08b10a55dc81a78e28cd96b310cc21e
Ahmed Farido <AhmedFarido@github.com>
2018-09-03 11:30:01 +0200

    first commit

---
+++ ikon/Extension/placeHolderColor.swift
@@ -0,0 +1,21 @@
+//
+//  placeHolderColor.swift
+//  ikon
+//
+//  Created by FARIDO on 9/2/18.
+//  Copyright © 2018 FARIDO. All rights reserved.
+//
+
+import UIKit
+
+class placeHolderColor: UITextField {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
