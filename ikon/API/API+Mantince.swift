1c677bc7f027bf1c6e3a0277a029e878ba20960a
Ahmed Farido <AhmedFarido@github.com>
2018-09-10 18:53:41 +0200

    first commit

---
+++ ikon/API/API+Mantince.swift
@@ -0,0 +1,9 @@
+//
+//  API+Mantince.swift
+//  ikon
+//
+//  Created by FARIDO on 9/7/18.
+//  Copyright © 2018 FARIDO. All rights reserved.
+//
+
+import Foundation
---
+++ ikon/Model/maintanceAPI.swift
@@ -0,0 +1,13 @@
+//
+//  maintanceAPI.swift
+//  ikon
+//
+//  Created by FARIDO on 9/7/18.
+//  Copyright © 2018 FARIDO. All rights reserved.
+//
+
+import UIKit
+
+class maintanceAPI: NSObject {
+
+}
